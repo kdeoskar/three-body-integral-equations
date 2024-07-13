b33bfb4c580fccbd639a3ae97dda2f8272c2325c
CXUANJ <lalala.xuanjie@gmail.com>
2024-07-12 17:47:37 -0700

    Create fitting.py

---
+++ fitting.py
@@ -0,0 +1,5 @@
+#fitting:
+#We have to create a list as ydata and N_list as the xdata list
+#Define the fitting models
+#params, covariance = curve_fit(nonlinear_func, x_data, y_data, p0=initial_guess)
+#params[0] = α 
